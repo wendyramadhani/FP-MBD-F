36f48fa6642eadce4f751430089216509ddb1e70
wendyramadhani <wgataramadhani28@gmail.com>
2024-06-26 13:33:25 +0700

    fix rawat inap controller

--- RS/be/controllers/rawatInapController.js
+++ RS/be/controllers/rawatInapController.js
@@ -22,9 +22,9 @@ const getRawatInapById = async (req, res, next) => {
 }
 
 const addRawatInap = async (req, res, next) => {
-    console.log(req);
+    console.log(req.body);
     const { nomor_kamar, status, tanggal_masuk, tanggal_keluar, diagnosis, pasien_id_pasien, dokter_id_dokter, resep_id_resep, pembayaran_id_pembayaran } = req.body;
-    if (!nomor_kamar || !status || !tanggal_masuk || !tanggal_keluar || !diagnosis || !pasien_id_pasien || !dokter_id_dokter) {
+    if (!nomor_kamar || !status || !tanggal_masuk || !pasien_id_pasien || !dokter_id_dokter || !diagnosis) {
         return res.status(400).json({ error: 'All fields must filled' });
     }
     
@@ -36,12 +36,12 @@ const addRawatInap = async (req, res, next) => {
         
         if (checkIdResVal) {
             const result = await rawatInapService.addData(rawat_inap);
-            res.status(200).json({ success: true, data: rawat_inap });
+            return res.status(200).json({ success: true, data: rawat_inap });
         } else {
-            res.status(404).json({ success: false, error: 'id_pasien not registered yet' });
+            return res.status(404).json({ success: false, error: 'id_pasien not registered yet' });
         }
     } catch (error) {
-        res.status(500).json(error);
+        return res.status(500).json(error);
     }
 }
 
